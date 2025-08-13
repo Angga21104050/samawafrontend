02275b932004232171b34868597fe1c65b4a784c
Angga21104050 <21104050@ittelkom-pwt.ac.id>
2025-08-13 14:10:19 +0700

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
+import { config } from "process";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "samawabackend.test",
+        port: "", // kosongkan kalau tidak ada port
+        pathname: "/api/**", // izinkan semua path
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  }
 };
 
 export default nextConfig;
---
+++ src/app/@modal/[...]/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { PreventScrolling } from "@/components/modal";
+import { RouterBack } from "@/components/modal";
+
+type Request = {
+    searchParams: {
+        modal: string;
+        [key: string]: string;
+    };
+};
+
+function page(request: Request) {
+    if (request.searchParams.modal && request.searchParams.modal !== "") {
+        return (
+            <>
+                <div className="fixed bg-black/80 z-50 inset-0 flex items-center justify-center">
+                    <div className="bg-white max-w-xl p-4 rounded-2xl min-h-44 relative z-20">
+                        {/*{}*/}
+                    </div>
+                    <RouterBack />
+                </div>
+                <PreventScrolling />
+            </>
+        )
+    }
+}
+
+export default page;
